fb7c39b8ff09d9d1f8855a588007f723eb70c99c
Syahril Zulkefli <syahzul@gmail.com>
2021-07-20 07:23:00 +0800

    Clean-up

--- stubs/resources/views/partials/dashboard/footer.blade.php
+++ stubs/resources/views/partials/dashboard/footer.blade.php
@@ -9,16 +9,13 @@
             <div class="col-lg-6">
                 <ul class="nav nav-footer justify-content-center justify-content-lg-end">
                     <li class="nav-item">
+                        <a href="https://akukoder.com" class="nav-link text-muted" target="_blank">AkuKoder</a>
+                    </li>
+                    <li class="nav-item">
                         <a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Creative Tim</a>
                     </li>
                     <li class="nav-item">
-                        <a href="https://www.creative-tim.com/presentation" class="nav-link text-muted" target="_blank">About Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="https://creative-tim.com/blog" class="nav-link text-muted" target="_blank">Blog</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted" target="_blank">License</a>
+                        <a href="https://github.com/zacksmash/fortify-ui" class="nav-link text-muted" target="_blank">Fortify UI</a>
                     </li>
                 </ul>
             </div>
